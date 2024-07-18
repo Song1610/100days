b1f604ea33e405b4a904caf23f8f630e1a222d5e
Song <twd5741@gmail.com>
2024-07-18 22:22:41 +0900

    docstring add.

--- Day 10/lecture/function_return.py
+++ Day 10/lecture/function_return.py
@@ -28,37 +28,44 @@ def my_function():
 output = result
 my_function() 호출 시 result 값 6이 출력
 '''
+
+
 # 1차
 def format_name():
-  f_name = input("성 : ")
-  l_name = input("이름 : ")
-  return f_name
-  return l_name
+    f_name = input("성 : ")
+    l_name = input("이름 : ")
+    return f_name
+    return l_name
 
 format_name()
+
 
 # 2차
 def format_name(f_name, l_name):
-  f_name = str(input("성: ")).title()
-  l_name = str(input("이름: ")).title()
-  return f_name
-  return l_name
+    f_name = str(input("성: ")).title()
+    l_name = str(input("이름: ")).title()
+    return f_name
+    return l_name
 
 format_name()
 
+
 # 3차
 def format_name(f_name, l_name):
-  print(f_name.title())
-  print(l_name.title())
-  
-format_name("kim","mingyu")
+    print(f_name.title())
+    print(l_name.title())
+
+
+format_name("kim", "mingyu")
+
 
 def format_name(f_name, l_name):
-  format_f_name = f_name.title()
-  format_l_name = l_name.title()
-  return f"{format_f_name}.{format_l_name}"
+    format_f_name = f_name.title()
+    format_l_name = l_name.title()
+    return f"{format_f_name}.{format_l_name}"
 
-format_string = format_name("kim","MINGYU")
+
+format_string = format_name("kim", "MINGYU")
 print(format_string)
 
 # 함수 하나가 리턴문을 가졌을 때
@@ -67,11 +74,28 @@ def 어쩌구():
   블라블라
   return ___ # return : 이 함수의 끝
 '''
+
+
 def format_name(f_name, l_name):
-  if f_name == "" or l_name == "":
-    return "잘못입력하셨습니다."
-  format_f_name = f_name.title()
-  format_l_name = l_name.title()
-  return f"result : {format_f_name}.{format_l_name}"
-  
+  """
+  take a first and last name and format it to return the title case version of the name.
+  """
+    if f_name == "" or l_name == "":
+        return "잘못입력하셨습니다."
+    format_f_name = f_name.title()
+    format_l_name = l_name.title()
+    return f"result : {format_f_name}.{format_l_name}"
+
+
 print(format_name(input("성 : "), input("이름 : ")))
+
+
+# docstring
+''' 
+함수나 다른 코드블록에서 코딩할 때 문서 만드는 방법
+그냥 쓰는 이거 ''' ''' 아니면 """ """를 쓰면 됨
+'''
+
+def svt(hp, vc, pp):
+   ''' svt is hiphop, vocal, performance.'''
+   # 이렇게 한 뒤 svt 하면 docsting이 뜸
