63e2112213fa453a82fc08a655e0aa13aa735d7e
Song1610 <twd5741@gmail.com>
2024-09-03 09:51:52 +0900

    수정

--- Day10/project/main.py
+++ Day10/project/main.py
@@ -1,1 +1,26 @@
 # calculator project
+import art
+print(art.logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+# Todo 1
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+# Todo 2
+operation = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+result = operation["*"](n1 = 4, n2 = 8)
+print(result)
