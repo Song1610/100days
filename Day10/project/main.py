416ae0a04dbf0736b9ccc9bce6ae2a7205311917
Song1610 <twd5741@gmail.com>
2024-09-04 16:40:54 +0900

    add project main.py

--- Day10/project/main.py
+++ Day10/project/main.py
@@ -1,6 +1,5 @@
 # calculator project
 import art
-print(art.logo)
 
 def add(n1, n2):
     return n1 + n2
@@ -14,13 +13,36 @@ def divide(n1, n2):
     return n1 / n2
 
 # Todo 2
-operation = {
+operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide
 }
 
+# Todo 3: 사전 연산을 사용하여 계산을 수행합니다. 사전을 사용하여 4 * 8을 곱합니다.
+# print(operations["*"](4,8))
 
-result = operation["*"](n1 = 4, n2 = 8)
-print(result)
+def calculator():
+    print(art.logo)
+    accumulate = True
+    num1 = float(input("first number : "))
+
+    while accumulate:
+        for symbol in operations:
+            print(symbol)
+        op = input("choice operations: ")
+        num2 = float(input("second number : "))
+
+        result = operations[op](num1,num2)
+        print(f"{num1} {op} {num2} = {result}")
+
+        choice = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if choice == "y":
+            num1 = result
+        else:
+            accumulate = False
+            print("\n" * 5)
+            calculator()
+
+calculator()
