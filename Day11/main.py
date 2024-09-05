86a329fca4e2f9fae4bde6ecf491826388425807
Song1610 <twd5741@gmail.com>
2024-09-05 16:18:41 +0900

    add day11

--- Day11/main.py
+++ Day11/main.py
@@ -1,1 +1,14 @@
 # blackjack
+import random
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.sample(cards, 2)
+    print(card)
+    return card
+
+
+user_cards = [deal_card()]
+computer_cards = [deal_card()]
+
+sum = sum(user_cards)
