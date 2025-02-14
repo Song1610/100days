a2d48e4c51b2147d118cd2d9e506d1655b0edfdd
Song1610 <twd5741@gmail.com>
2025-02-14 10:35:55 +0900

    day4/project/main.py

--- Day 4/project/main.py
+++ Day 4/project/main.py
@@ -1,1 +1,44 @@
 # 25-02-13 작성
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+image = [rock, paper, scissors]
+
+question = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+
+user = image[question]
+computer = random.choice(image)
+print("user:")
+print(user)
+print("computer:")
+print(computer)
+
+if user == computer:
+    print("It's a draw!")
